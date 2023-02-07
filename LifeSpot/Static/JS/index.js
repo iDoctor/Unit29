6ea0180c1eb09454748d6cbcddbeb4d8f813c657
Roman Filatov <ikardoc@gmail.com>
2023-02-07 19:20:45 +0300

    js-функции

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -41,18 +41,59 @@ function handleSession() {
     session.set("age", prompt("Пожалуйста, введите ваш возраст"));
 
     // Проверка на возраст и сохранение сессии
-    if (session.get("age") >= 18) {
-        let startDate = new Date().toLocaleString();
+    if (window.sessionStorage.getItem("userAge") == null) {
+        let input = prompt("Пожалуйста, введите ваш возраст");
+        window.sessionStorage.setItem("userAge", input);
 
-        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-        session.set("startDate", startDate);
+        checker(true);
+    } else {
+        checker(false);
     }
-    else {
-        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-        window.location.href = "http://www.google.com";
-    }
+
     // Вывод в консоль
+    logger();
     for (let result of session) {
         console.log(result);
     }
+}
+
+//Проверка возраста
+let checker = function (newVisit) {
+    if (window.sessionStorage.getItem("userAge") >= 18) {
+        // Добавим проверку на первое посещение, чтобы не показывать приветствие
+        // лишний раз
+        if (newVisit) {
+            alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+        }
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+function showTime(city, timeZone) {
+    let time = document.getElementById(city);
+    let h = new Date().getUTCHours();
+    let m = new Date().getUTCMinutes();
+    let s = new Date().getUTCSeconds();
+
+    if (h + timeZone > 23 || h + timeZone < 0)
+        h = Math.abs(h + timeZone - 24);
+    else
+        h = h + timeZone;
+    if (h < 10)
+        h = '0' + h;
+    if (m < 10)
+        m = '0' + m;
+    if (s < 10)
+        s = '0' + s;
+    time.innerHTML = h + ":" + m + ":" + s;
+}
+
+//Вывод в консоль
+let logger = function () {
+    console.log('Начало сессии: ' + window.sessionStorage.getItem("startDate"));
+    console.log('Даныне клиента: ' + window.sessionStorage.getItem("userAgent"));
+    console.log('Возраст пользователя: ' + window.sessionStorage.getItem("userAge"));
 }
